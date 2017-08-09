a3c68f73c8d6b88b04e354488901cd4fb590a4a0
lc <530405769@qq.com>
2017-08-09 16:40:25 +0800

    first commit

---
+++ AlertViewDemo/Alert/BaseAlertViewComponent.swift
@@ -0,0 +1,21 @@
+//
+//  BaseAlertViewComponent.swift
+//  AlertViewDemo
+//
+//  Created by leecong on 2017/8/9.
+//  Copyright © 2017年 leecong. All rights reserved.
+//
+
+import UIKit
+
+class BaseAlertViewComponent: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ AlertViewDemo/BaseAlertViewCofig.swift
@@ -0,0 +1,13 @@
+//
+//  BaseAlertViewCofig.swift
+//  AlertViewDemo
+//
+//  Created by leecong on 2017/8/9.
+//  Copyright © 2017年 leecong. All rights reserved.
+//
+
+import UIKit
+
+class BaseAlertViewCofig: NSObject {
+
+}
